11d83c0c19313ec1a5e311cd64837125a07fed2b
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-01-21 07:29:55 +0530

    Profile page done

--- app/src/main/java/com/example/humors/connect/ResultsActivity.java
+++ app/src/main/java/com/example/humors/connect/ResultsActivity.java
@@ -2,17 +2,21 @@ package com.example.humors.connect;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.example.humors.R;
+import com.example.humors.home.ProfileActivity;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
@@ -26,6 +30,7 @@ public class ResultsActivity extends AppCompatActivity {
     private List<DonutSection> healthStatus = new ArrayList<>();
     private DonutProgressView donutProgressView;
     private FrameLayout bottomSheet;
+    private ImageButton profileButton;
 
     private BottomSheetBehavior bottomSheetBehavior = new BottomSheetBehavior();
 
@@ -49,6 +54,7 @@ public class ResultsActivity extends AppCompatActivity {
 
         donutProgressView = findViewById(R.id.health_status_your_results);
         bottomSheet = findViewById(R.id.bottom_sheet_your_results);
+        profileButton = findViewById(R.id.profile_button);
 
         healthStatus.add(new DonutSection("user_health", Color.parseColor("#FAFF00"), 75.0F));
         donutProgressView.setCap(100);
@@ -71,7 +77,7 @@ public class ResultsActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
-
+        profileButton.setOnClickListener(view -> startActivity(ProfileActivity.newInstance(this)));
     }
 
     private void setObservers() {
--- app/src/main/java/com/example/humors/home/ProfileActivity.java
+++ app/src/main/java/com/example/humors/home/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.humors.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.humors.R;
@@ -12,5 +14,39 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        init();
+    }
+
+    private void init() {
+        initialiseVariables();
+        fetchData();
+        setViews();
+        setListeners();
+        setObservers();
+    }
+
+    private void initialiseVariables() {
+
+    }
+
+    private void fetchData() {
+
+    }
+
+    private void setViews() {
+
+    }
+
+    private void setListeners() {
+
+    }
+
+    private void setObservers() {
+
+    }
+
+    public static Intent newInstance(Context context) {
+        return new Intent(context, ProfileActivity.class);
     }
 }
